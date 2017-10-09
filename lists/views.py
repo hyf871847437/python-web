f51f00e306ff6c0a8b5a838ed7f033d09d60d7a6
fengge <871847437@qq.com>
2017-10-09 17:04:05 +0800

    chapter6.3

--- lists/views.py
+++ lists/views.py
@@ -3,9 +3,6 @@ from django.http import HttpResponse
 from lists.models import Item,List
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
 
     return render(request,'home.html')
 
